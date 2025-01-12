2a2e41d8d38121e8f5d541408e0d6061f8b6d9cf
Mario-Gozie <oguedoihucg@gmail.com>
2025-01-13 00:25:13 +0200

    worked on making event delegation to implement smooth scroll

--- Js/controller.js
+++ Js/controller.js
@@ -3,6 +3,7 @@ import view from "./view.js";
 const menuOpen = document.querySelector(".menu__button");
 const menuClose = document.querySelector(".close__button");
 const sidebar = document.querySelector(".sidebar");
+const navbar = document.querySelector(".nav__bar");
 
 // Opeining Menu
 menuOpen.addEventListener("click", function (e) {
@@ -14,4 +15,15 @@ menuOpen.addEventListener("click", function (e) {
 menuClose.addEventListener("click", function (e) {
   e.preventDefault();
   sidebar.classList.remove("sidebar__active");
+});
+
+// Activating Smooth Scrolling this is done with event bubbling
+
+navbar.addEventListener("click", function (e) {
+  e.preventDefault();
+  // console.log(e.target);
+  if (e.target.classList.contains("nav__item")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behaviour: "smooth" });
+  }
 });
