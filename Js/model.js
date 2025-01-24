5971561257d9805d334d4227e7214b82cdf70622
Mario-Gozie <oguedoihucg@gmail.com>
2025-01-25 01:01:54 +0200

    working on the model part of the javascript code

---
+++ Js/model.js
@@ -0,0 +1,50 @@
+const state = {
+  customers: [],
+  newsletter: [],
+};
+
+// MAIN DATA WEBSITE CODE
+
+// exporting the newsletter updater
+export function updateList(item) {
+  state.newsletter.push(item);
+}
+
+// PORTAL DATA CODE
+
+class Client {
+  constructor(
+    id,
+    firstName,
+    lastName,
+    address,
+    phoneNumber,
+    username,
+    email,
+    password,
+    gender
+  ) {
+    this.id = id;
+    this.firstname = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phone_Number = phoneNumber;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.gender = gender;
+  }
+}
+
+// exporting client Data
+export const client = new Client(
+  id,
+  firstName,
+  lastName,
+  address,
+  phoneNumber,
+  username,
+  email,
+  password,
+  gender
+);
