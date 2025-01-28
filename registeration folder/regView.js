14084a075d55cc5de9c6e9ecf23db8bc846af673
Mario-Gozie <oguedoihucg@gmail.com>
2025-01-28 19:47:07 +0200

    working on registeration

---
+++ registeration folder/regView.js
@@ -0,0 +1,19 @@
+class regView {
+  constructor() {
+    this.registerationForm = document.getElementById("Registeration__form");
+  }
+}
+
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+// const btnCloseModal = document.querySelector(".close__modal");
+// const registerationForm = document.getElementById("Registeration__form");
+
+// console.log(modal, overlay, btnCloseModal, registerationForm);
+
+// registerationForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   modal.classList.remove("hidden__modal");
+//   overlay.classList.remove("hidden__overlay");
+//   alert("money");
+// });
