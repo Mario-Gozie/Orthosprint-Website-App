e65dc14e67d517e08ac95889cac38a18120901cb
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-03 00:03:12 +0200

    Registration now works with external script

--- registeration folder/regView.js
+++ registeration folder/regView.js
@@ -1,79 +1,64 @@
-// class regView {
-//   constructor() {
-//     this.registerationForm = document.getElementById("Registeration__form");
-//   }
+const registerationForm = document.querySelector(".RegisterationForm");
+const popUpContainer = document.querySelector(".popup-container");
+const CloseRegModal = document.querySelector(".closeModal");
+const messageContainer = document.querySelector(".message-container");
+console.log(CloseRegModal);
 
-//   regClient(handler) {
-//     this.registerationForm.addEventListener("submit", function (e) {
-//       e.preventDefault();
-//       const data = new FormData(this);
-//       console.log(data);
+console.log(registerationForm);
 
-//       const firstName = data.get("firstName");
-//       const lastName = data.get("lastName");
-//       const address = data.get("address");
-//       const phoneNumber = data.get("phoneNumber");
-//       const username = data.get("username");
-//       const email = data.get("email");
-//       const password = data.get("password");
-//       const confirmPassword = data.get("confirmPassword");
+const generateHTML = function (name) {
+  return `
+  <h2>Thank You!</h2>
+    <p>
+      We now have your details <span>${name}</span>. You can now login.
+    </p>`;
+};
 
-//       const gender = data.get("gender");
+const closePopup = function () {
+  CloseRegModal.addEventListener("click", function (e) {
+    e.preventDefault();
+    popUpContainer.classList.remove("Open-popup");
+    window.location.href = "../Orthosprint App/indexA.html";
+  });
+};
 
-//       if (password !== confirmPassword) {
-//         alert("Passwords do not match. Please try again");
-//         return;
-//       }
+closePopup();
 
-//       //get the newID
-//       // use a constructor function
+const customers = [];
 
-//       handler(
-//         firstName,
-//         lastName,
-//         address,
-//         phoneNumber,
-//         username,
-//         email,
-//         password,
-//         gender
-//       );
-//     });
-//   }
-// }
+console.log(customers);
+registerationForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(this);
+  console.log(data);
 
-// export default new regView();
+  const firstName = data.get("firstName");
+  const lastName = data.get("lastName");
+  const address = data.get("address");
+  const phoneNumber = data.get("phoneNumber");
+  const username = data.get("username");
+  const email = data.get("email");
+  const password = data.get("password");
+  const confirmPassword = data.get("confirmPassword");
 
-// registerationForm.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   const data = new FormData(this);
-//   console.log(data);
+  const gender = data.get("gender");
 
-//   const firstName = data.get("firstName");
-//   const lastName = data.get("lastName");
-//   const address = data.get("address");
-//   const phoneNumber = data.get("phoneNumber");
-//   const username = data.get("username");
-//   const email = data.get("email");
-//   const password = data.get("password");
-//   const confirmPassword = data.get("confirmPassword");
+  if (password !== confirmPassword) {
+    alert("Passwords do not match. Please try again");
+    return;
+  }
 
-//   const gender = data.get("gender");
+  customers.push({
+    firstName,
+    lastName,
+    address,
+    phoneNumber,
+    username,
+    email,
+    password,
+  });
 
-//   if (password !== confirmPassword) {
-//     alert("Passwords do not match. Please try again");
-//     return;
-//   }
-
-//   customers.push({
-//     firstName,
-//     lastName,
-//     address,
-//     phoneNumber,
-//     username,
-//     email,
-//     password,
-//   });
-
-//   console.log(customers);
-// });
+  messageContainer.innerHTML = generateHTML(firstName);
+  popUpContainer.classList.add("Open-popup");
+  console.log(customers);
+});
