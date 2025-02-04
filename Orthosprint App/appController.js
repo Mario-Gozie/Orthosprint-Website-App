068920806ee515c3a98e30afdef2e31cc482a7a3
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-04 05:04:38 +0200

    will sort out the form data issue by daybreak

---
+++ Orthosprint App/appController.js
@@ -0,0 +1,4 @@
+import { state } from "../Js/model.js";
+import login from "./login.js";
+
+login.OnloginEvent();
---
+++ Orthosprint App/login.js
@@ -0,0 +1,27 @@
+class Login {
+  usernameEmailValue = document.querySelector(".username");
+
+  password = document.querySelector(".password");
+
+  loginForm = document.querySelector(".login-form");
+
+  constructor() {
+    this.OnloginEvent();
+  }
+
+  OnloginEvent() {
+    this.loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(this.loginForm);
+
+      const formData = new FormData(this.loginForm);
+      console.log(formData);
+
+      //   formData.forEach((value, key) => {
+      //     console.log(`${key}:${value}`);
+      //   });
+    });
+  }
+}
+
+export default new Login();
