7ec500fb4a6c690b980fb9d9705e6e5e45ba06f6
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-19 03:45:32 +0200

    code looking better

--- Orthosprint App/appointmentView.js
+++ Orthosprint App/appointmentView.js
@@ -3,8 +3,13 @@ class AppointmentView {
 
   renderAppointments(appoinmentArray, Element) {
     if (appoinmentArray.length === 0 || !appoinmentArray) {
-      const html = `All appointment will be here, lets get started`;
+      const html = `<strong> All appointment will be here, lets get started </strong>`;
       Element.innerHTML = html;
+      return;
+    }
+
+    if (appoinmentArray.length > 0) {
+      appoinmentArray.map((appt) => {});
     }
   }
 }
