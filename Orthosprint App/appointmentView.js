288d08710bc9d79fb32da57aaa0f0b02426c3588
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-19 03:28:35 +0200

    making a few changes with rendering appointment.

--- Orthosprint App/appointmentView.js
+++ Orthosprint App/appointmentView.js
@@ -1,3 +1,10 @@
 class AppointmentView {
   constructor() {}
+
+  renderAppointments(appoinmentArray, Element) {
+    if (appoinmentArray.length === 0 || !appoinmentArray) {
+      const html = `All appointment will be here, lets get started`;
+      Element.innerHTML = html;
+    }
+  }
 }
