4b12c99854084f8b2085fa6d0a144897091cc592
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-26 17:55:45 +0200

    code works fairly

--- Orthosprint App/appController.js
+++ Orthosprint App/appController.js
@@ -209,8 +209,20 @@ export default class appController {
   handleTimeClick(event) {
     if (event.target.classList.contains("time-button")) {
       event.preventDefault();
-      console.log("button clicked", event.target);
-      event.target.classList.add("selected-time");
+      if (event.target.classList.contains("selected-time")) {
+        event.target.classList.remove("selected-time");
+        this.selectedTime = null;
+        console.log(this.selectedTime);
+      } else {
+        const allTimeButtons =
+          this.timeContainer.querySelectorAll(".time-button");
+        allTimeButtons.forEach((btn) => btn.classList.remove("selected-time"));
+
+        console.log("button clicked", event.target);
+        event.target.classList.add("selected-time");
+        this.selectedTime = event.target.value;
+        console.log(this.selectedTime);
+      }
     }
   }
 
