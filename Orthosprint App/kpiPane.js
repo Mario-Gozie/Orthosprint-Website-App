de55a6a2847ab589137cb8a3becdb5cefa08680c
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-28 20:28:52 +0200

    modified the KPI code a bit

--- Orthosprint App/kpiPane.js
+++ Orthosprint App/kpiPane.js
@@ -22,6 +22,24 @@ export default class KPIpane {
         <p>-</p>
       </div>`;
       this.kpiWrapper.innerHTML = html;
+    } else {
+      const html = `<div class="kpi-container honoured-appointments">
+        <h3>Honoured Appointments</h3>
+        <p>${this.renderBasedOnStatus(userBookingArray, "honoured")}</p>
+      </div>
+      <div class="kpi-container cancellled-appointments">
+        <h3>Canceled Appointments</h3>
+        <p>${this.renderBasedOnStatus(userBookingArray, "canceled")}</p>
+      </div>
+      <div class="kpi-container pending-appointments">
+        <h3>Pending Appointments</h3>
+        <p>${this.renderBasedOnStatus(userBookingArray, "pending")}</p>
+      </div>
+      <div class="kpi-container missed-appointments">
+        <h3>Missed Appointments</h3>
+        <p>${this.renderBasedOnStatus(userBookingArray, "missed")}</p>
+      </div>`;
+      this.kpiWrapper.innerHTML = html;
     }
   }
 
