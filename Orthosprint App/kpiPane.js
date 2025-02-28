c20580887d8fe93b68902ae8b374b6e6916ebf7a
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-28 03:18:20 +0200

    working on the KPI section

--- Orthosprint App/kpiPane.js
+++ Orthosprint App/kpiPane.js
@@ -1,1 +1,24 @@
- 
+export default class KPIpane {
+  constructor() {}
+
+  renderKPIs(userBookingArray) {
+    if (userBookingArray.length === 0 || !userBookingArray) {
+      const html = `<div class="kpi-container honoured-appointments">
+        <h3>Honoured Appointments</h3>
+        <p>45</p>
+      </div>
+      <div class="kpi-container cancellled-appointments">
+        <h3>Canceled Appointments</h3>
+        <p>12</p>
+      </div>
+      <div class="kpi-container pending-appointments">
+        <h3>Pending Appointments</h3>
+        <p>8</p>
+      </div>
+      <div class="kpi-container missed-appointments">
+        <h3>Missed Appointments</h3>
+        <p>5</p>
+      </div>`;
+    }
+  }
+}
