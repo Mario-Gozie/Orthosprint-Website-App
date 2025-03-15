f07689c3c04fb21fac68261768e83cf6c13a89a2
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-15 08:49:02 +0200

    working on the view

--- Js/view.js
+++ Js/view.js
@@ -1,6 +1,20 @@
 class view {
-  menuOpen = document.querySelector(".menu__button");
-  menuClose = document.querySelector(".close__button");
+  constructor() {
+    this.menuOpen = document.querySelector(".menu__button");
+    this.menuClose = document.querySelector(".close__button");
+
+    // Opeining Menu
+    this.menuOpen.addEventListener("click", function (e) {
+      e.preventDefault();
+      sidebar.classList.add("sidebar__active");
+    });
+
+    // Closing Menu
+    this.menuClose.addEventListener("click", function (e) {
+      e.preventDefault();
+      sidebar.classList.remove("sidebar__active");
+    });
+  }
 }
 
 export default new view();
