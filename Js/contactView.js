173e260c3aa66c971acda64dee37ea5d96e46fe8
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-15 09:06:03 +0200

    working a bit on modal window

---
+++ Js/contactView.js
@@ -0,0 +1,6 @@
+export default class ContactView {
+  constructor(controller) {
+    this.controller = controller;
+    this.contactForm = document.querySelector(".contact_form");
+  }
+}
---
+++ Js/modalWindowView.js
@@ -0,0 +1,9 @@
+export default class ModalWindowView {
+  constructor() {
+    this.modalWindow = document.getElementById("modal-container");
+    this.modalBtn = document.querySelector(".modal-button");
+    this.popupMesageContainer = document.querySelector(
+      ".popup-message-container"
+    );
+  }
+}
