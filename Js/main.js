8247ee61ececb85dd950314c3d6a8ec33f3f5cb6
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-16 14:45:49 +0200

    worked on intersection API and understood that DOMcontentLoaded event listener can only be called once

--- Js/main.js
+++ Js/main.js
@@ -2,4 +2,14 @@ import Controller from "./controller.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Controller(); // Initialize the controller
+
+  // window.addEventListener("load", () => {
+  //   if (window.location.hash) {
+  //     // Scroll to the top of the page
+  //     window.scrollTo(0, 0);
+
+  //     // Optionally, remove the hash from the URL
+  //     history.replaceState(null, null, " ");
+  //   }
+  // });
 });
