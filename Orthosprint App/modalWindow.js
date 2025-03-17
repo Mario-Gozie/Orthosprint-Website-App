a4712408cfd5ed214e85208efd6ec8e6b1800755
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-18 00:59:49 +0200

    I have been able to create the Object for Modal window

--- Orthosprint App/modalWindow.js
+++ Orthosprint App/modalWindow.js
@@ -1,3 +1,20 @@
 export default class ModalWindow {
-  constructor() {}
+  constructor() {
+    this.modalWindowContainer = document.querySelector(".popUp-container");
+  }
+
+  openModalWindow(htmlElements) {
+    this.modalWindowContainer.classList.add("show-popUp"); // Add class to show the popup
+    this.modalWindowContainer.innerHTML = htmlElements;
+
+    // Set a timeout to hide the modal after 3 seconds (3000 milliseconds)
+    setTimeout(() => {
+      this.closeModalWindow(); // Call the function to hide the modal
+    }, 3000);
+  }
+
+  closeModalWindow() {
+    this.modalWindowContainer.classList.remove("show-popUp"); // Remove class to hide the popup
+    this.modalWindowContainer.innerHTML = ""; // Optional: clear the inner HTML if desired
+  }
 }
