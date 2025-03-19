a34be3c6742af6e20fc72adabb282a4a6d0f2900
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-19 23:01:33 +0200

    working on the woring on the modal window

--- Orthosprint App/modalWindow.js
+++ Orthosprint App/modalWindow.js
@@ -1,11 +1,12 @@
 export default class ModalWindow {
   constructor() {
     this.modalWindowContainer = document.querySelector(".popUp-container");
+    this.messageIcon = this.modalWindowContainer.querySelector("message-icon");
   }
 
   openModalWindow(htmlElements) {
     this.modalWindowContainer.classList.add("show-popUp"); // Add class to show the popup
-    this.modalWindowContainer.innerHTML = htmlElements;
+    this.messageIcon.innerHTML = htmlElements;
 
     // Set a timeout to hide the modal after 3 seconds (3000 milliseconds)
     setTimeout(() => {
